435d3e174aca166ce01176bb1477834e64a484eb
The_EnG1nE <baHHbaSh@gmail.com>
2024-02-24 13:56:53 +0500

    dt update

--- RPG/game.js
+++ RPG/game.js
@@ -11,7 +11,7 @@ function IsTouching(ax,ay,aw,ah, bx,by,bw,bh){
 	return true;
 }
 function ObjTouching(obj1, obj2){
-    IsTouching(obj1.x, obj1.y, obj1.w)
+    return IsTouching(obj1.x, obj1.y, obj1.w, obj1.h, obj2.x, obj2.y, obj2.w, obj2.h);
 }
 TileId = [
     "Grass",
@@ -32,6 +32,14 @@ A = false
 S = false
 D = false
 Shift = false
+
+let dt = 0;
+
+let LastTime = 0;
+
+function GetTime(){
+    return Date.now() / 1000
+}
 
 function ImageLoad(path){
     let a = new Image()
@@ -74,48 +82,76 @@ class Game{
         document.body.appendChild(this.canvas);
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
-
+        this.ScreenObject = new Object(0, 0, window.innerWidth, window.innerHeight);
+        
         this.Player = new PlayerObject();
         
         this.MapObjects = [];
         this.MapRange = [];
         fetch("/RPG/Map").then(Response => {return Response.text();})
         .then(data => {
-                let tmp = data.split("\n");
-                tmp.forEach(
-                    (row)=>{
-                        this.MapRange.push(row.split(" "))
-                    }
+            let tmp = data.split("\n");
+            tmp.forEach(
+                (row)=>{
+                    this.MapRange.push(row.split(" "))
+                }
                 );
                 Application.MapBuild();
             });
-
-        let GameObjects = [
-            this.Player
-        ]
-        this.GameObjects = GameObjects;
-
-        setInterval(this.Update, 1000/60);
-    }
-    MapBuild(){
-        let map = Application.MapRange;
-        console.log(map)
-        for(let y=0; y < map.length; y++){
-            let tmp = []
-            for(let x=0; x < map[y].length; x++){
-                tmp.push(new MapObject(x, y, map[y][x]));
-            }
-            Application.MapObjects.push(tmp);
+            
+            let GameObjects = [
+                this.Player
+            ]
+            this.GameObjects = GameObjects;
+            
+            LastTime = GetTime()
+            setInterval(this.Update, 1000/60);
         }
+        MapBuild(){
+            let map = Application.MapRange;
+            console.log(map)
+            for(let y=0; y < map.length; y++){
+                let tmp = []
+                for(let x=0; x < map[y].length; x++){
+                    tmp.push(new MapObject(x, y, map[y][x]));
+                }
+                Application.MapObjects.push(tmp);
+            }
     }
     Update(){
-        Application.Player.Move()
+        dt = GetTime() - LastTime;
+        LastTime = GetTime();
+        Application.Player.BPos.x = Application.Player.x;
+        Application.Player.BPos.y = Application.Player.y;
+
+        Application.Player.MoveX();
+        Application.MapObjects.forEach((list)=>{list.forEach((obj)=>{
+            if (!obj.GetCollsion()) return
+            if (ObjTouching(Application.Player, obj)){
+                Application.Player.x = Application.Player.BPos.x;
+            }
+        })})
+        Application.Player.MoveY();
+        Application.MapObjects.forEach((list)=>{list.forEach((obj)=>{
+            if (!obj.GetCollsion()) return
+            if (ObjTouching(Application.Player, obj)){
+                Application.Player.y = Application.Player.BPos.y;
+            }
+        })})
+        
         Application.ctx.clearRect(0, 0, Application.canvas.width, Application.canvas.height);
 
         Application.ctx.save();
 
         Application.ctx.translate(-(Application.Player.x - Application.canvas.width/2 + 25), -(Application.Player.y - Application.canvas.height/2 + 50));
-        Application.MapObjects.forEach((list)=>{list.forEach((obj)=>{obj.Render(Application.ctx)})})
+
+        Application.ScreenObject.pos = Array((Application.Player.x - Application.canvas.width/2 + 25),(Application.Player.y - Application.canvas.height/2 + 50))
+
+        Application.MapObjects.forEach((list)=>{list.forEach((obj)=>{
+            if (ObjTouching(obj, Application.ScreenObject)){
+                obj.Render(Application.ctx)
+            }
+        })})
 
         Application.GameObjects.forEach((obj)=>{obj.Render(Application.ctx)})
         Application.ctx.restore();
@@ -139,6 +175,13 @@ class Object{
     }
     set h(value){
         this.height = value;
+    }
+    get pos(){
+        return Array(this.x, this.y)
+    }
+    set pos(value){
+        this.x = value[0];
+        this.y = value[1];
     }
 }
 
@@ -167,29 +210,44 @@ class MapObject extends ImageObject{
     constructor(CellX, CellY, id){
         let CSize = 100
         super(CellX * CSize++, CellY * CSize++, CSize, CSize, TileImages[id])
+        this.id = id
     }
     SetId(NewId){
+        this.id = NewId;
         this.SetImage(TileImages[NewId]);
+    }
+    GetCollsion(){
+        if((this.id >= 2 && this.id < 5) || (this.id >= 6)){
+            return true
+        }
+        return false
     }
 }
 
 class PlayerObject extends ImageObject{
     constructor(SpawnPoint){
         super(0, 0, 50, 100, "https://encycolorpedia.ru/00008b.png");
-        this.Speed = 3
+        this.BPos = {x: 0, y: 0}
+        this.Speed = 100
     }
-    Move(){
+    MoveX(){
         if (Shift){
-            if(W){this.y-=this.Speed}
-            if(A){this.x-=this.Speed}
-            if(S){this.y+=this.Speed}
-            if(D){this.x+=this.Speed}
+            if(A){this.x-=this.Speed * dt;}
+            if(D){this.x+=this.Speed * dt;}
         }
         else{
-            if(W){this.y-=this.Speed * 2}
-            if(A){this.x-=this.Speed * 2}
-            if(S){this.y+=this.Speed * 2}
-            if(D){this.x+=this.Speed * 2}
+            if(A){this.x-=this.Speed * 2 * dt;}
+            if(D){this.x+=this.Speed * 2 * dt;} 
+        }
+    }
+    MoveY(){
+        if (Shift){
+            if(W){this.y-=this.Speed * dt;}
+            if(S){this.y+=this.Speed * dt;}
+        }
+        else{
+            if(W){this.y-=this.Speed * 2 * dt;}
+            if(S){this.y+=this.Speed * 2 * dt;}
         }
     }
 }
